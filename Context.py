39b37980cdc6064b70d9a337ec1e652fc7322dac
isaacpitblado <isaacpitblado@tamu.edu>
2021-03-06 17:33:19 -0600

    staging area for unittests

--- Context.py
+++ Context.py
@@ -4,11 +4,19 @@ class ContextType(Enum):
   LIST_TRANSACTIONS = "transactions"
   LIST_TICKERS = "list_tickers"
   FILE_NAME = "file_name"
+  INTERACTIVE = "interactive"
 
 
 class Context:
   
-  def __init__(self, parsed_args):
+  def __init__(self, parsed_args = None):
+    if parsed_args:
+      self.__init__from_parsed_args(parsed_args)
+
+    else:
+      pass
+
+  def __init__from_parsed_args(self, parsed_args):
     self.context = {}
     i = 0
     context_keys = [a for a in dir(parsed_args) if not a.startswith('_')]
